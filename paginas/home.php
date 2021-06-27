fb86c82188f3df43a9ddb3670f7957c6daacea12
Aritz Pozo <aritz.pozo@gmail.com>
2021-06-27 13:12:40 -0500

    sadasd

--- paginas/home.php
+++ paginas/home.php
@@ -1,3 +1,10 @@
-<h1>PAGINA DE EJEMPLO</h1>
-<p>Este es un párrrafo</p>
-asdasdasdasdasd
+<section>
+    Introducción al proyecto
+    Este sitio está preparado para ayudar en la educación de niños y adoscentes en primaria y secunaria....
+</section>
+<section>
+    Top cursos
+</section>
+<section>
+    Ultimos recursos
+</section>
---
+++ paginas/login.php
@@ -0,0 +1,13 @@
+<section>
+    Usuario o correo
+    <input>
+    Contraseña
+    <input>
+    Recordar usuario
+    <div class='boton'>identificarse</div>
+</section>
+
+<section>
+    ¿Olvidaste tu pass?
+    Crear cuenta                
+</section>
