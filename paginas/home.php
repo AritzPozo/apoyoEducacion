475a565d2c851566b55fb956f42e6900b3efdeb0
Aaron Plata <arnplatah@gmail.com>
2021-07-18 06:40:33 -0500

    estructura html

--- paginas/home.php
+++ paginas/home.php
@@ -1,10 +1,39 @@
-<section>
-    Introducción al proyecto
-    Este sitio está preparado para ayudar en la educación de niños y adoscentes en primaria y secunaria....
-</section>
-<section>
-    Top cursos
-</section>
-<section>
-    Ultimos recursos
-</section>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+    <link rel="icon" href="assets/nmnFavico.ico">
+    <link href="styles/variables.css" rel="stylesheet">
+    <link href="styles/responsive.css" rel="stylesheet">
+
+    
+</head>
+<body >
+
+    <div class="header">
+        <div class="logo"></div>
+        <a href="">Blog</a>
+        <a href="">Foro</a>
+        <a href="">Profesionales</a>
+        <div class="btnUsuario"></div>
+    </div>
+
+    <div class="sliderHome">
+        <div class="imgSliderHome"></div>
+        <div class="descSliderHome">
+            <div class="TituloSec">Último Articulo</div>
+            <div class="descSec">
+                
+            </div>
+        </div>
+    </div>
+
+ 
+    <script type="text/javascript" src="scripts/paginasServicios.js"></script>
+    
+
+</body>
+</html>
